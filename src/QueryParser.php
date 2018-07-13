50f21255156435d8bc370f1d712ab430e2ab6e9a
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2018-07-13 16:01:17 +0900

    Simplify query parser

--- src/QueryParser.php
+++ src/QueryParser.php
@@ -10,24 +10,10 @@ final class QueryParser {
 
     protected static $parser = null;
 
-    public static function parse($path) {
+    public static function parse($code) {
         if (!isset(static::$parser)) {
             static::$parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
         }
-
-        if (is_dir($path)) {
-            foreach (Utility::findByExt($path, 'php') as $p) {
-                yield static::parseFile($p);
-            }
-
-        } else {
-            yield static::parseFile($path);
-
-        }
-    }
-
-    private static function parseFile($path) {
-        $code = @file_get_contents($path) or die("Failed to get contents of $path");
 
         try {
             return static::$parser->parse('<?php ' . $code);
@@ -37,4 +23,5 @@ final class QueryParser {
 
         }
     }
+
 }

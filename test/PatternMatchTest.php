69509e653633f48952770e4b6e419730518650cc
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2019-04-14 19:00:32 +0900

    Add the skelton of PatternMatchTest

--- test/PatternMatchTest.php
+++ test/PatternMatchTest.php
@@ -6,6 +6,12 @@ use Phinder\PhpParser;
 
 class PatternMatchTest extends TestCase
 {
+    private static $_CASES = [
+        '_' => [
+            true => ['var_dump(1);'],
+        ],
+    ];
+
     private $_patternParser;
 
     private $_phpParser;
@@ -14,5 +20,30 @@ class PatternMatchTest extends TestCase
     {
         $this->_patternParser = new PatternParser();
         $this->_phpParser = new PhpParser();
+    }
+
+    /**
+     * @dataProvider provider
+     */
+    public function testMatch($pattern, $php, $match)
+    {
+        $patAst = $this->_patternParser->parse($pattern);
+        $phpAst = $this->_phpParser->parse("<?php $php");
+        $this->assertSame($patAst->match($phpAst), $match);
+    }
+
+    public function provider()
+    {
+        $array = [];
+
+        foreach (self::$_CASES as $pattern => $tests) {
+            foreach ($tests as $match => $phps) {
+                foreach ($phps as $php) {
+                    $array[] = [$pattern, $php, $match ? true : false];
+                }
+            }
+        }
+
+        return $array;
     }
 }

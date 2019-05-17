5b6c50faa7e769766066fc8cfdca4f52a71ab812
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2019-05-17 10:30:08 +0900

    Update php-lex

--- php-lex/lib/regex.php
+++ php-lex/lib/regex.php
@@ -7,7 +7,7 @@ function buildRegex($tokens)
     $regs = ['\t+|\s+'];
     foreach ($tokens as $token) {
         $n = $token['name'];
-        $r = escape($token['expr'], '"');
+        $r = escape($token['expr'], "'");
         $r = escape($r, '/');
         $r = trim($r);
         $regs[] = "(?<$n>$r)";
